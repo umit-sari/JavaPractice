2afc4ad81af31ae5130c85b471be5c71df080c44
ümit sari <umitsari1986@hotmail.com>
2023-08-12 18:53:18 +0300

    PracticeDay_01

---
+++ src/PracticeDay_01/C01_odev.java
@@ -0,0 +1,35 @@
+package PracticeDay_01;
+
+public class C01_odev {
+    public static void main(String[] args) {
+        /*
+        ÖDEV SORUSU: Aşağıdaki gibi bir ATM Karşılama Ekranı Hazırlayıp yazdırınız
+        **********  WISE BANK   **********
+            WISE ATM'YE HOŞGELDİNİZ
+            1 – Bakiye Sorgulama
+            2 – Para Yatırma
+            3 – Para Çekme
+            4 – Bilgi Güncelleme
+            5 – Kart İade
+        **********  WISE BANK   **********
+         */
+
+        String s1 ="**********";
+        String s2=" WISE BANK";
+        String s3="WISE ATM'YE HOŞGELDİNİZ";
+        String s4="1 – Bakiye Sorgulama";
+        String s5="2 – Para Yatırma";
+        String s6="3 – Para Çekme";
+        String s7="4 – Bilgi Güncelleme";
+        String s8="5 – Kart İade";
+        char ch1=' ';
+
+        System.out.println(s1+ch1+s2+ch1+ch1+s1+
+                "\n"+ch1+ch1+s3+"\n"+ch1+ch1+s4+"\n"+ch1+ch1+s5+"\n"+ch1+ch1+s6+
+                "\n"+ch1+ch1+s7+"\n"+ch1+ch1+s8+"\n"+s1+ch1+s2+ch1+ch1+s1);
+
+
+
+
+    }
+}
